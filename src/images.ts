58e9fece7bf2f2506e687bb38da338b1c13127b5
d38u6 <d38u6dev@gmail.com>
2020-10-11 17:30:34 +0200

    add basic pictures page

---
+++ src/images.ts
@@ -0,0 +1,23 @@
+import images from "../public/images/*.jpg";
+
+function createImage(src: string, alt: string): HTMLAnchorElement {
+  const a = document.createElement("a");
+  a.href = `game?images=${src.slice(1)}`;
+
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = alt;
+
+  a.appendChild(img);
+
+  return a;
+}
+
+function renderImages(parentEl: HTMLDivElement): void {
+  (Object.entries(images) as [string, string][]).forEach(([alt, imageSrc]) => {
+    const img = createImage(imageSrc, alt);
+    parentEl.appendChild(img);
+  });
+}
+
+renderImages(document.querySelector(".images") as HTMLDivElement);
